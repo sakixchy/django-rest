00f90c693e24a971d868dfdc1776f38f2ff6626e
Saki Chy <sakibchowdhury79@gmail.com>
2024-05-07 08:02:39 +0000

    add image filter for posts

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import  Response 
 from .models import Profile
 from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 class ProfileList(APIView):
     def get(self, request):
@@ -14,9 +15,11 @@ class ProfileList(APIView):
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
